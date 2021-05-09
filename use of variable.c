7861e300f07e03fda25d18f7e7fb5a5603fd4b2e
Nikhil96-droid <83655398+Nikhil96-droid@users.noreply.github.com>
2021-05-09 16:58:19 +0530

    Create use of variable.c

---
+++ use of variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+   int var2;
+   int var3;
+   printf("Enter a number %d", var2);
+   printf("Enter a number %d", var2);
+   scanf("%d and %d", &var2,&var3);
+   return 0;
+
+}

8becd21b3683f45217e5a4e6e4d2b338cf31bc23
Nikhil96-droid <83655398+Nikhil96-droid@users.noreply.github.com>
2021-05-09 22:30:19 +0530

    Update Add two integer.c

--- Add two integer.c
+++ Add two integer.c
@@ -4,9 +4,11 @@ int main()
 
    int var2;
    int var3;
+   int var4;
    var2=60;
    var3=40;
-   printf("Value of var2 is %d and var3 is %d", var2, var3);
+   var4=var2+var3;
+   printf("Sum of %d and %d is %d", var2, var3,var4);
    return 0;
 
 }

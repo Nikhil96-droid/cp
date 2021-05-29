133e5111a3a7e4c0d2a5b3127e17770aaee5e2af
Nikhil96-droid <83655398+Nikhil96-droid@users.noreply.github.com>
2021-05-29 18:56:42 +0530

    Create Odd even.c

---
+++ Odd even.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+int main()
+{
+       int a;
+    printf("This is designed by Nikhil Bhardwaj");
+    printf("\nEnter value of a:");
+    scanf("%d",&a);
+    if(a%2==0) 
+    {
+    printf("A is even number");
+    }
+    else
+    {
+    printf("A is odd number");
+    }
+    return 0;
+}
+ 
